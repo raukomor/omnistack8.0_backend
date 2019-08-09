85265612adf51a5cdf7c27ef2d4169fdc7da59b6
Raukomor <rodrigo.corneta@hotmail.com>
2019-08-09 11:11:23 -0300

    adicionando websocket e middle-ware para a construção do match

--- src/server.js
+++ src/server.js
@@ -5,15 +5,50 @@ const cors = require('Cors');
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {
+    
+}
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    // connectedUsers[ID_USUARIO] = socket.id;
+ 
+    //console.log(user, socket.id);
+
+    connectedUsers[user] = socket.id;
+
+    //escutando o frontend
+    // socket.on('hello', message =>{
+    //     console.log(message)
+    // })
+
+    //mandando msg pro front
+    // setTimeout(() => {
+    //     socket.emit('world', {
+    //         message: 'OmniStack'
+    //     });
+    // }, 5000)
+});
+
 
 //Conexão com o banco
 mongoose.connect('mongodb+srv://raukomor:raukomor@cluster0-nn7il.mongodb.net/oministack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
